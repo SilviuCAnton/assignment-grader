15a7a313ddd3670eee3a9c23e3a59f4fd19bec70
SilviuCAnton <silviu.c.anton@gmail.com>
2019-11-02 12:41:01 +0200

    Defined the services for the Student and the Assignment entities.

---
+++ src/main/java/services/AssignmentService.java
@@ -0,0 +1,95 @@
+package services;
+
+import domain.Assignment;
+import exceptions.ValidationException;
+import repositories.AssignmentFileRepository;
+import utils.Constants;
+
+/**
+ *
+ */
+public class AssignmentService {
+    private AssignmentFileRepository assignmentFileRepository;
+
+    /**
+     * stores an assignment in the repository
+     * @param id - id of the assignment - int
+     * @param description - description of the assignment - String
+     * @param deadlineWeek - deadline week of the assignment - int
+     * @return the result of the storing operation - String
+     * @throws ValidationException if the assignment is not valid
+     * @throws IllegalArgumentException if the assignment is null
+     */
+    public String addAssignment(int id, String description, int deadlineWeek) throws ValidationException, IllegalArgumentException {
+        String res;
+        Assignment assignment = assignmentFileRepository.save(new Assignment(id, description, deadlineWeek, Constants.yearStructure));
+        if (assignment != null) {
+            res = "Assignment already exists: " + assignment.toString();
+        } else {
+            res = "The assignment has been stored.";
+        }
+        return res;
+    }
+
+    /**
+     * updates an assignment in the repository
+     * @param id - id of the assignment - int
+     * @param description - description of the assignment - String
+     * @param deadlineWeek - deadline week of the assignment - int
+     * @return the result of the updating operation - String
+     * @throws ValidationException if the assignment is not valid
+     * @throws IllegalArgumentException if the assignment is null
+     */
+    public String updateAssignment(int id, String description, int deadlineWeek) throws ValidationException, IllegalArgumentException {
+        String res;
+        Assignment assignment = assignmentFileRepository.update(new Assignment(id, description, deadlineWeek, Constants.yearStructure));
+        if (assignment != null) {
+            res = "The assignment " + assignment.getDescription() + " has been updated.";
+        } else {
+            res = "The assignment with the given id does not exist.";
+        }
+        return res;
+    }
+
+    /**
+     * finds an assignment in the repository
+     * @param id - the id of the assignment - int
+     * @return the result of the search operation - String
+     */
+    public String findAssignment(int id) {
+        String res;
+        Assignment assignment = assignmentFileRepository.findOne(id);
+        if (assignment != null) {
+            res = assignment.toString();
+        } else {
+            res = "The assignment with the given id does not exist.";
+        }
+        return res;
+    }
+
+    /**
+     * returns all assignments
+     * @return list of all assignments - String
+     */
+    public String findAllAssignments() {
+        StringBuilder sb = new StringBuilder();
+        assignmentFileRepository.findAll().forEach(x-> sb.append(x.toString()).append('\n'));
+        return sb.toString();
+    }
+
+    /**
+     * deletes an assignment from the repository
+     * @param id - id of the assignment - int
+     * @return the result of the deletion operation - String
+     */
+    public String deleteAssignment(int id) {
+        String res;
+        Assignment assignment = assignmentFileRepository.delete(id);
+        if (assignment != null) {
+            res = assignment.getDescription() + " has been removed.";
+        } else {
+            res = "The assignment with the given id does not exist.";
+        }
+        return res;
+    }
+}
---
+++ src/main/java/services/StudentService.java
@@ -0,0 +1,124 @@
+package services;
+
+import domain.Student;
+import domain.validators.StudentValidator;
+import exceptions.ValidationException;
+import repositories.StudentFileRepository;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Service for student operations
+ */
+public class StudentService {
+    private StudentFileRepository studentRepo;
+
+    public StudentService(String fileName) {
+        this.studentRepo = new StudentFileRepository(new StudentValidator(), fileName);
+    }
+
+    /**
+     * returns the student repository
+     * @return studentRepo - StudentFileRepository
+     */
+    public StudentFileRepository getStudentRepo() {
+        return studentRepo;
+    }
+
+    /**
+     * sets the student repository
+     * @param studentRepo - StudentFileRepository
+     */
+    public void setStudentRepo(StudentFileRepository studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    /**
+     * saves a student in the repository
+     * @param id - id of the student - String
+     * @param firstName - first name of the student - String
+     * @param lastName - last name of the student - String
+     * @param email - email of the student - String
+     * @param coordinator - coordinator of the student - String
+     * @return the result of the storing operation - String
+     * @throws IllegalArgumentException if the student to be stored is null
+     * @throws ValidationException - if the student is not valid
+     */
+    public String saveStudent(String id, String firstName, String lastName, String email, String coordinator) throws IllegalArgumentException, ValidationException {
+        String result;
+        Student st = studentRepo.save(new Student(id, firstName, lastName, email, coordinator));
+        if (st != null) {
+            result = "Student already exists: " + st.toString();
+        } else {
+            result = "The student has been stored.";
+        }
+        return result;
+    }
+
+    /**
+     * updates a student in the repository
+     * @param id - id of the student - String
+     * @param firstName - first name of the student - String
+     * @param lastName - last name of the student - String
+     * @param email - email of the student - String
+     * @param coordinator - coordinator of the student - String
+     * @return the result of the storing operation - String
+     * @throws IllegalArgumentException if the student to be updated is null
+     * @throws ValidationException if the student is not valid
+     */
+    public String updateStudent(String id, String firstName, String lastName, String email, String coordinator) throws IllegalArgumentException, ValidationException {
+        String result;
+        Student st = studentRepo.update(new Student(id, firstName, lastName, email, coordinator));
+        if (st != null) {
+            result = "The student " + st.getFirstName() + ' ' + st.getLastName() + " has been updated.";
+        } else {
+            result = "The student with the given id does not exist.";
+        }
+        return result;
+    }
+
+    /**
+     * finds a student in the repository
+     * @param id - id of the student to be found - int
+     * @return the result of searchind the student - String
+     * @throws IllegalArgumentException if the id is null
+     */
+    public String findStudent(String id) throws IllegalArgumentException{
+        String res;
+        Student st = studentRepo.findOne(id);
+        if (st != null) {
+            res = st.toString();
+        } else {
+            res = "The student with the given id does not exist.";
+        }
+        return res;
+    }
+
+    /**
+     * returns all students
+     * @return student list - String
+     */
+    public String findAllStudents() {
+        StringBuilder sb = new StringBuilder();
+        studentRepo.findAll().forEach(x-> sb.append(x.toString()).append('\n'));
+        return sb.toString();
+    }
+
+    /**
+     * deletes a student from the repository
+     * @param id - id of the student to be deleted - int
+     * @return the result of the deletion operation - String
+     * @throws IllegalArgumentException if the id is null
+     */
+    public String deleteStudent(String id) throws IllegalArgumentException {
+        String res;
+        Student st = studentRepo.delete(id);
+        if (st != null) {
+            res = st.getFirstName() + " " + st.getLastName() + " has been removed.";
+        } else {
+            res ="The student with the given id does not exist.";
+        }
+        return res;
+    }
+
+}
